d4df34b0d2fcc31514922610ad8fb07c88cef824
Krzysztof Radomski <k_radomski@tlen.pl>
2017-09-02 15:17:28 +0200

    'changes'

--- src/App.js
+++ src/App.js
@@ -1,20 +1,156 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Shout from './newShout';
+//import configBase from './configBase';
 
-class App extends Component {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      listOfShouts: [],
+      loaded: false,
+      whoId: null,
+      whoName: "anonymous",
+      now: Date.now(),
+      expiration: 24,
+      refShoutbox: "",
+      keyName: "",
+      firebase: global.firebase,
+      firebaseui: global.firebaseui
+    };
+
+  }
+
+  configBase() {
+    let config = {
+      apiKey: "AIzaSyANmT2lWop_rbnLuXcl2V13Izjg3H3ybaY",
+      authDomain: "messageboard1-6c745.firebaseapp.com",
+      databaseURL: "https://messageboard1-6c745.firebaseio.com",
+      projectId: "messageboard1-6c745",
+      storageBucket: "messageboard1-6c745.appspot.com",
+      messagingSenderId: "515471374408"
+    };
+    let firebase = this.state.firebase;
+    firebase.initializeApp(config);
+    let uiConfig = {
+      signInSuccessUrl: "http://localhost:3000/",
+      signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+      ]
+    };
+    /* initialize firebase auth */
+    const ui = new this.state.firebaseui.auth.AuthUI(
+      this.state.firebase.auth()
+    );
+    /* the start method will wait until the DOM is loaded */
+    ui.start("#firebaseui-auth-container", uiConfig);
+    let deadline = this.state.now - this.state.expiration * 60 * 60 * 1000;
+    this.setState({
+      refShoutbox: this.state.firebase
+        .database()
+        .ref("/shoutbox")
+        .orderByChild("timestamp")
+        .endAt(deadline)
+    });
+  }
+
+  purgeOldDatabaseDataAndDrawNew(name, ref) {
+    let deleted = 0;
+    ref
+      .once("value", function(snap) {
+        snap.forEach(function(item) {
+          item.ref.remove();
+          deleted = deleted + 1;
+        });
+        console.log(
+          "Total outdated data removed from " + name + ": " + deleted
+        );
+      })
+      .then(function() {
+        deleted !== 0
+          ? console.log("Finished purging old data from: " + name)
+          : console.log("..." + name + " up to date, no data purged...");
+      });
+  }
+
+  hideProgress() {
+    if (
+      this.state.loaded === false &&
+      document.querySelector(".loader") !== null
+    ) {
+      setTimeout(function() {
+        document.querySelector(".loader").style.opacity = 0;
+        document.querySelector(".loader").style.display = "none";
+      }, 500);
+    }
+    this.setState({
+      loaded: true
+    });
+  }
+
+  componentWillMount() {
+    console.log('Starting app online...')
+    this.configBase();
+  }
+
+  componentDidMount() {
+    this.purgeOldDatabaseDataAndDrawNew("shoutbox", this.state.refShoutbox);
+    const rootRef = this.state.firebase.database().ref().child("/shoutbox").orderByChild("timestamp").limitToLast(10);
+
+    rootRef.on("child_added", snap => {
+      const previousList = this.state.listOfShouts;
+      previousList.push({
+        timestamp: snap.val().timestamp,
+        shout: snap.val().shout,
+        whoName: snap.val().whoName
+      });
+      this.setState({
+        listOfShouts: previousList
+      });
+      document.querySelectorAll(".shout:last-of-type")[0].scrollIntoView();
+
+      this.hideProgress();
+    });
+  }
+
+  componentDidUpdate() {
+    document.querySelectorAll(".shout:last-of-type")[0].scrollIntoView();
+  }
+
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+    const listOfShouts = this.state.listOfShouts.map((item, i) =>
+      <div className="shout" key={i}>
+        <p>
+          {new Date(item.timestamp).toLocaleTimeString()} on {new Date(item.timestamp).toLocaleDateString()}, by {item.whoName}
         </p>
+        {" "}
+        <h1> {item.shout} </h1>
+        {" "}
       </div>
     );
+    return (<div>
+    <div className="container board-container">
+      <div className="board-title">
+       <div id="firebaseui-auth-container"> </div>
+        <div id="sign-in-status"> </div> <div id="sign-in"> </div>
+        <div id="account-details"> </div> <h1> Chuttup </h1>
+        <div className="loader" id="ldr" ></div>
+        <div id="shoutbox" className="tab-pane">
+          <form className="form-horizontal" id="shoutboxForm">
+            <div id="shoutbox-inner"> <div> {listOfShouts} </div> </div>
+
+            <Shout state = {this.state}/>
+          </form>
+
+        </div>
+
+        <footer> Made by KR. </footer>
+
+      </div>
+
+    </div>
+  </div>);
   }
 }
 
--- src/newShout.js
+++ src/newShout.js
@@ -1,63 +1,69 @@
-class NeWShout extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            shout: ""
-        };
-        this.handleChange.bind(this);
-        this.handleSubmit.bind(this);
-    }
+import React from 'react';
+import './App.css';
 
-    handleSubmit(event) {
-        let shout = this.state.shout;
-        let handle = "anon";
-        let who = handle || whoName;
-        let timestamp = Date.now();
-        whoId = whoId || "anonymous";
-        let data = {
-            shout: shout,
-            timestamp: timestamp,
-            whoId: whoId,
-            whoName: whoName,
-            handle: handle
-        };
-        let pushShout = firebase.database().ref("/shoutbox").push(data);
-        console.log("Creating shout...");
-        console.log(who);
-        event.preventDefault();
-    }
+class Shout extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      shout: "",
+    };
+  }
 
-    handleChange(event) {
-        this.setState({ shout: event.target.value });
-    }
+  handleSubmit(event) {
+    let firebase =  this.props.state.firebase;
+    let shout = this.state.shout;
+    let handle = "anon";
+    let who = this.props.state.handle || this.props.state.whoName;
+    let timestamp = Date.now();
+    let whoId = this.props.state.whoId || "anonymous";
+    let data = {
+      shout: shout,
+      timestamp: timestamp,
+      whoId: this.props.state.whoId,
+      whoName: this.props.state.whoName,
+      handle: handle
+    };
+    firebase.database().ref("/shoutbox").push(data);
+    console.log("Creating shout...");
+    console.log(who);
+    this.setState({
+      shout: ''
+    });
+    event.preventDefault();
+  }
 
-    render() {
-        return (<div>
-            <label htmlFor="handle" className="control-label"></label>
-                <div>
-                    <input
-                        type="text"
-                        className="form-control required"
-                        id="shout"
-                        name="shout"
-                        placeholder="start typing to enable realtime"
-                        value={this.state.shout}
-                        onChange={this.handleChange.bind(this)}
-                        required
-                    />
-                    <input
-                        id="submitShoutbox"
-                        name="submit"
-                        type="submit"
-                        value="Send"
-                        onClick={this.handleSubmit.bind(this)}
-                    />
-                    {" "}
-                    {" "}
-                </div>
-            </div>
-        );
-    }
+  handleChange(event) {
+    this.setState({
+      shout: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <label htmlFor="handle" className="control-label"> </label>
+        <div>
+          <input
+            type="text"
+            className="form-control required"
+            id="shout"
+            name="shout"
+            placeholder="start typing to enable realtime"
+            value={this.state.shout}
+            onChange={this.handleChange.bind(this)}
+            required
+          />
+          <input
+            id="submitShoutbox"
+            name="submit"
+            type="submit"
+            value="Send"
+            onClick={this.handleSubmit.bind(this)}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default NeWShout;
+export default Shout;
